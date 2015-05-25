c2fdd5c4a2907093ac54cf473a3dbde4797901af
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-05-25 00:59:18 -0400

    added success and failure images and cleaned up scripts.js code.

--- scripts.js
+++ scripts.js
@@ -1,210 +1,300 @@
 // set global values
 var randVal;
 var input;
+var won = false;
+var lost = false;
 var totalGuesses = 5;
 var guessesLeft = totalGuesses;
 var guesses = [];
 var message = $('<div role="alert" style="margin-top: 25px"></div>');
 var compMessage = $('<h3></h3>');
 
-// when loaded, initialize the randVal
+// when loaded, initialize the randVal and the guesses, and focus on the input bar
 $(document).ready(function () {
-	randVal = Math.ceil(Math.random() * 100);
-	$('#guessesRemaining').text(guessesLeft);
+	setRandVal();
+	setGuesses();
 	$('#playersGuess').focus();
-});
 
-// new game button listener
-$('#newGame').click(function () {
-	playAgain();
-});
+	// new game button listener
+	$('#newGame').on('click', playAgain);
 
-// resets page and the necessary variables
-function playAgain() {
-	randVal = Math.floor(Math.random() * 100);
-	guessesLeft = totalGuesses;
-	guesses = [];
-	message.remove();
-	compMessage.text('').remove();
-	$('.list-group').empty();
-	$('.title').text("Try to guess the number!");
-	$('.lead').text("Input a number between 1 and 100 in the field below, then update your guess based on the given feedback.");
-	$('#guessesRemaining').text(guessesLeft);
-	$('#submit').prop('disabled', false);
-	$('#playersGuess').val('').prop('disabled', false).focus();
-};
+	// hint button listener
+	$('#hint').on('click', function () {
+		alert('The number is: '+ randVal);
+	});
 
-// hint button listener
-$('#hint').click(function () {
-	alert('The number is: '+randVal);
-});
+	// submit key click listener
+	$('#submit').on('click', runGame);
 
-// when enter key is pressed
-$('#playersGuess').keypress(function (e) {
-	if (e.which === 13)
-		runGame();
-});
+	// input bar enter key listener
+	$('#playersGuess').keypress(function (e) {
+		if (e.which === 13)
+			runGame();
+	});
 
-// submit key click listener
-$('#submit').click(function (e) {
-	runGame();
-});
+	// submit button / input bar script
+	function runGame() {
+		// store the input and reset the input field
+		var input = parseInt($('#playersGuess').val());
+		$('#playersGuess').val('');
 
-// submit button script
-function runGame() {
-	// store the input and reset the input field
-	input = parseInt($('#playersGuess').val());
-	$('#playersGuess').val('');
+		// check if guess is valid
+		if (!isValid(input)) {
+			alert("Please enter a value between 1 and 100");
+			return;
+		}
 
-	// check if guess is valid
-	if (!isValid(input)) {
-		alert("Please enter a value between 1 and 100");
-		return;
-	}
+		// check if already guessed this value
+		if (isRepeat(input)) {
+			alert("You have already guessed that number");
+			return;
+		}
 
-	// check if already guessed this value
-	if (isRepeat(input)) {
-		alert("You have already guessed that number");
-		return;
-	}
+		// add the guess to array
+		guesses.push(input);
 
-	// add the guess to array
-	guesses.push(input);
+		// update guesses left
+		guessesLeft -= 1;
+		setGuesses();
 
-	// update guesses left
-	guessesLeft -= 1;
-	$('#guessesRemaining').text(guessesLeft);
+		// compare to old guess
+		$('#input-container').append(compMessage);
+		if (guesses.length > 1)
+			isCloser(input) ? compMessage.text('Getting warmer!') : compMessage.text('Getting colder');
 
-	// update record of old guesses, with appropriate colors
-	var oldG = $("<li class='list-group-item'><span id='val'></span><span id='comp'></span></li>");
-	if (randVal === input) {
-		oldG.addClass('list-group-item-success');
-	}
-	else if (isWarm(guesses[guesses.length-1])) {
-		if (isHot(guesses[guesses.length-1])) 
-			oldG.addClass('list-group-item-danger');
-		else
-			oldG.addClass('list-group-item-warning');
-	}
-	else {
-		oldG.addClass('list-group-item-info');
-	}
-	$('.list-group').prepend(oldG);
-	$('#val').text(guesses[guesses.length-1]);
+		// replace old comparison message
+		message.removeClass();
+		$('#input-container').append(message);
 
-	// compare to old guess
-	$('#input-container').append(compMessage);
-	if (guesses.length > 1)
-		isCloser(input) ? compMessage.text('Getting warmer!') : compMessage.text('Getting colder');
+		// update record of old guesses and update comparison message, with appropriate colors
+		var guessLog = $("<li class='list-group-item'><span id='val'></span><span id='comp'></span></li>");
+		var closeness = howClose(input);
+		var tooLow = randVal > input;
+		// add new log onto the list of past guesses
+		$('.list-group').prepend(guessLog);
+		$('#val').text(input);
+		// set colors and messages based on how close the guess is
+		if (randVal === input) {
+			// set entry color to green and update its text
+			guessLog.addClass('list-group-item-success');
+			$('#comp').text(' - correct!');
+			// run game won script
+			gameWon();
+			return;
+		}
+		else if (closeness === 'hot') {
+			// set color to red
+			guessColor('red', guessLog);
+			// if too low
+			if (tooLow) {
+				message.text('Really close - Guess a little higher!');
+				$('#comp').text(' - a little too low');
+			}
+			// if too high
+			else {
+				message.text('Really close - Guess a little lower!');
+				$('#comp').text(' - a little too high');
+			}
+		}
+		// if within 5
+		else if (closeness === 'warm') {
+			// set color to yellow
+			guessColor('yellow', guessLog);
+			// if too low
+			if (tooLow) {
+				message.text('Warm - Guess higher!');
+				$('#comp').text(' - too low');
+			}
+			// if too high
+			else {
+				message.text('Warm - Guess lower!');
+				$('#comp').text(' - too high');
+			}
+		}
+		// if within 50
+		else if (closeness === "cold") {
+			// set color to blue
+			guessColor('blue', guessLog);
+			// if too low
+			if (tooLow) {
+				message.text('Cold - Guess higher!');
+				$('#comp').text(' - too low');
+			}
+			// if too high
+			else {
+				message.text('Cold - Guess lower!');
+				$('#comp').text(' - too high');
+			}
+		}
+		// if not within 50
+		else {
+			// set color to blue
+			guessColor('blue', guessLog);
+			// if too low
+			if (tooLow) {
+				message.text('Very Cold - Guess much higher!');
+				$('#comp').text(' - way too low');
+			}
+			// if too high
+			else {
+				message.text('Very Cold - Guess much lower!');
+				$('#comp').text(' - way too high');
+			}
+		}
 
-	// replace old alert
-	message.removeClass();
-	$('#input-container').append(message);
+		// check for Game Over
+		if (guessesLeft === 0) {
+			// run game over script
+			gameOver();
+			return;
+		}
+	};
 
-	// compare guess to randVal
-	if (input === randVal) {
-		// set alert to Green and say you won!!
-		message.text('You got it!!');
-		message.addClass('alert alert-success');
-		$('.title').text("You won!");
-		$('.lead').text("Congratulations! Press the 'Play Again' button to try again.");
-		$('#comp').text(' - correct!');
+	// resets page and the necessary variables
+	function playAgain() {
+		// reset random value, number of guesses, and list of previous guesses
+		setRandVal();
+		guessesLeft = totalGuesses;
+		guesses = [];
+		setGuesses();
+		// remove images, if displayed
+		if (won) {
+			toggleSuccessImage();
+			won = false;
+		}
+		if (lost) {
+			toggleFailureImage();
+			lost = false;
+		}
+		// remove comparison messages
+		message.remove();
+		compMessage.text('').remove();
+		// remove list of old guesses
+		$('.list-group').empty();
+		// reset title and lead text
+		$('.title').text("Try to guess the number!");
+		$('.lead').text("Input a number between 1 and 100 in the field below, then update your guess based on the given feedback.");
+		// enable submit button and input field, and focus on the input field
+		$('#submit').prop('disabled', false);
+		$('#playersGuess').prop('disabled', false).focus();
+	};
+
+	// triggered when the guess is correct
+	function gameWon() {
+		// display success image
+		toggleSuccessImage();
+		// disable submit options
 		disableSubmit();
-		compMessage.remove();
-		return;
-	}
-	else if (Math.abs(randVal - input) <= 5) {
-		// set alert to Red and say you are very hot
-		message.addClass('alert alert-danger');
-		if (randVal > input) {
-			message.text('Really close - Guess a little higher!');
-			$('#comp').text(' - a little too low');
-		}
-		else {
-			message.text('Really close - Guess a little lower!');
-			$('#comp').text(' - a little too high');
-		}
-	}
-	else if (Math.abs(randVal - input) <= 20) {
-		// change alert color to yellow and say you are close
-		message.addClass('alert alert-warning');
-		if (randVal > input) {
-			message.text('Warm - Guess higher!');
-			$('#comp').text(' - too low');
-		}
-		else {
-			message.text('Warm - Guess lower!');
-			$('#comp').text(' - too high');
-		}
-	}
-	else if (Math.abs(randVal - input) <= 50) {
-		// change alert to blue and say you are cold
-		message.addClass('alert alert-info');
-		if (randVal > input) {
-			message.text('Cold - Guess higher!');
-			$('#comp').text(' - too low');
-		}
-		else {
-			message.text('Cold - Guess lower!');
-			$('#comp').text(' - too high');
-		}
-	}
-	else {
-		message.addClass('alert alert-info');
-		if (randVal > input) {
-			message.text('Very Cold - Guess much higher!');
-			$('#comp').text(' - way too low');
-		}
-		else {
-			message.text('Very Cold - Guess much lower!');
-			$('#comp').text(' - way too high');
-		}
-	}
+		// update comparison message
+		messageUpdate('green', 'You got it!!');
+		// update title and lead text
+		$('.title').text("You won!");
+		$('.lead').text("Congratulations! Press the 'Play Again' button to try again.");	
+	};
 
-	// check for Game Over
-	if (guessesLeft === 0) {
+	// triggered when the user runs out of guesses
+	function gameOver() {
+		// display failure image
+		toggleFailureImage();
+		// disable submit options
+		disableSubmit();
+		// update comparison message
+		messageUpdate('blue', 'You ran out of guesses. Don\'t give up - try again!');
+		// update title and lead text
 		$('.title').text("Game Over");
 		$('.lead').text("Press 'Play Again' button to try again.");
-		message.text('You ran out of guesses. Don\'t give up - try again!');
+	};
+
+	// returns how hot or cold the guess is
+	function howClose(val) {
+		var dif = Math.abs(randVal - val);
+		if (dif === 0)
+			return 'correct';
+		else if (dif <= 5)
+			return 'hot';
+		else if (dif <= 20)
+			return 'warm';
+		else if (dif <= 50)
+			return 'cold';
+		else
+			return 'very cold';
+	};
+
+	// resets the message color, and updates its text
+	function messageUpdate(color, text) {
 		message.removeClass();
-		message.addClass('alert alert-info');
-		disableSubmit();
-		compMessage.remove();
-	}
-};
+		if (color === 'blue')
+			message.addClass('alert alert-info');
+		else if (color === 'green')
+			message.addClass('alert alert-success');
+		else if (color === 'red')
+			message.addClass('alert alert-danger');
+		else
+			message.addClass('alert alert-warning');
+		message.text(text);
+	};
 
-// returns true if the val was "warm" and false otherwise
-function isWarm(val) {
-	if (Math.abs(randVal - input) <= 20)
-		return true;
-	return false;
-};
+	// sets the comparison message and guess entry color
+	function guessColor(color, guessLog) {
+		if (color === 'blue') {
+			message.addClass('alert alert-info');
+			guessLog.addClass('list-group-item-info');
+		}
+		else if (color === 'green') {
+			message.addClass('alert alert-success');
+			guessLog.addClass('list-group-item-success');
+		}
+		else if (color === 'red') {
+			message.addClass('alert alert-danger');
+			guessLog.addClass('list-group-item-danger');
+		}
+		else {
+			message.addClass('alert alert-warning');
+			guessLog.addClass('list-group-item-warning');
+		}
+	};
 
-// returns true if the val was "hot" and false otherwise
-function isHot(val) {
-	if (Math.abs(randVal - input) <= 5)
-		return true;
-	return false;
-};
+	// true if val is in the prev array
+	function isRepeat(val) {
+		return (guesses.indexOf(val) !== -1);
+	};
 
-// true if val is in the prev array
-function isRepeat(val) {
-	return (guesses.indexOf(val) !== -1);
-}
+	// returns true if the input is valid
+	function isValid(val) {
+		return $.isNumeric(val) && val <= 100 && val >= 1;
+	};
 
-// returns true if the input is valid
-function isValid(val) {
-	return $.isNumeric(val) && val <= 100 && val >= 1;
-}
+	// disables the submit button and focuses on Play Again button
+	function disableSubmit() {
+		$('#submit').prop('disabled', true);
+		$('#playersGuess').prop('disabled', true);
+		$('#newGame').focus();
+	};
 
-// disables the submit button and focuses on Play Again button
-function disableSubmit() {
-	$('#submit').prop('disabled', true);
-	$('#playersGuess').prop('disabled', true);
-	$('#newGame').focus();
-}
+	// true if current guess is closer to randVal
+	function isCloser(val) {
+		return (Math.abs(randVal - val) < Math.abs(randVal - guesses[guesses.length-2]));
+	};
 
-// true if current guess is closer to randVal
-function isCloser(val) {
-	return (Math.abs(randVal - val) < Math.abs(randVal - guesses[guesses.length-2]));
-}
+	// sets a new random value
+	function setRandVal() {
+		randVal = Math.ceil(Math.random() * 100);
+	};
+
+	// updates the displayed number of guesses left
+	function setGuesses() {
+		$('#guessesRemaining').text(guessesLeft);
+	};
+
+	// toggles display of success image, and updates won state
+	function toggleSuccessImage() {
+		$('.success').find('img').slideToggle();
+		won = true;
+	};
+
+	// toggles display of failure image, and updates lost state
+	function toggleFailureImage() {
+		$('.failure').find('img').slideToggle();
+		lost = true;
+	};
+});
